6954266b914551f8eb576fd3c29ce3a521e44892
Treinamento <Treinamento@RD723939.raiadrogasil.com>
2020-02-27 15:29:11 -0300

    subindo aplicação

---
+++ src/componentes/contador/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react' ;
+
+import {Button} from 'reactstrap' ;
+import './contador.css'
+
+class Contador extends Component{
+    state = {
+        numero:0
+      }
+      
+      aumentar =() => {
+        let novoNumero = this.state.numero 
+        novoNumero++ 
+        this.setState({ numero:novoNumero})
+      }
+      diminuir =() => {
+        let novoNumero = this.state.numero 
+        if (novoNumero ===0) {return alert ('hoje nao faro')}
+        novoNumero-- 
+        this.setState({ numero:novoNumero})
+      }
+      
+        render () {
+          return (
+            <div>
+            <h1 className="contador-titulo">{this.state.numero}</h1>
+                <Button className = "mr-2" color="success" outline onClick={this.aumentar}>
+                    Aumentar
+                </Button>
+                <Button color="danger" outline onClick={this.diminuir}>
+                    Diminuir
+                </Button>
+            </div>
+          )
+        }
+}
+
+
+export default Contador;
